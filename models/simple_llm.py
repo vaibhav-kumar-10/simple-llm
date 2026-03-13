dba2c15a242a4271686fea24c8f91e4608c422c4
Remag501-Desktop <evanverma2003@gmail.com>
2026-03-12 20:38:34 -0700

    initial commit

---
+++ models/simple_llm.py
@@ -0,0 +1,9 @@
+import torch
+
+class SimpleLLM (nn.Module):
+    def __init__(self):
+        super(SimpleLLM, self).__init__()
+
+    def forward(self, x):
+        # Placeholder for the forward pass
+        return x
