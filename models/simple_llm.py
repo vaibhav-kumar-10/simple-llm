42d52e259c3a438f65886ae4f91071c81ff2f78b
Remag501-Desktop <evanverma2003@gmail.com>
2026-03-14 19:53:29 -0700

    feat: added tokenizer using tiktoken and decoder layer

--- models/simple_llm.py
+++ models/simple_llm.py
@@ -1,9 +1,43 @@
 import torch
+import torch.nn as nn
+import tiktoken
 
-class SimpleLLM (nn.Module):
+class SimpleLLM(nn.Module):
+
+    vocab_size = 50257
+    embedding_dim = 768
+
     def __init__(self):
         super(SimpleLLM, self).__init__()
+        self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
+        self.decoder = nn.Linear(self.embedding_dim, self.vocab_size)
+
+    def attention(self, x):
+        # Placeholder for the attention mechanism
+        # TODO: Implement a simple attention mechanism
+        return x
+    
+    def tokenizer(self, text):
+        # Placeholder for the tokenizer
+        # Splits text up into suface forms
+        # Calls downloaded map to convert surface forms to tokens
+
+        # Use tiktoken to tokenize the input text
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        x = torch.tensor(tokens).unsqueeze(0)
+        return x
+    
+    def embed_tokens(self, tokens):
+        # Placeholder for the embedding layer
+        # Runs tokens through a single layer to get embeddings
+        # Forward pass implementation should be simple linear transformation (nn.Embedding handles performance)
+        return self.embedding(tokens)
 
     def forward(self, x):
         # Placeholder for the forward pass
+        x = self.tokenizer(x)
+        x = self.embed_tokens(x)
+        x = self.attention(x)
+        x = self.decoder(x)
         return x
