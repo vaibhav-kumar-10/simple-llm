13790c946363fbd923be08bf47225f49ce47506b
Remag501-Desktop <evanverma2003@gmail.com>
2026-03-16 13:14:21 -0700

    refactor: attention block is its own class for modularity

--- models/simple_llm.py
+++ models/simple_llm.py
@@ -13,41 +13,9 @@ class SimpleLLM(nn.Module):
         super(SimpleLLM, self).__init__()
         self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
 
-        # Define the weight layers HERE so they are saved/trained
-        self.key_layer = nn.Linear(self.embedding_dim, self.embedding_dim)
-        self.query_layer = nn.Linear(self.embedding_dim, self.embedding_dim)
-        self.value_layer = nn.Linear(self.embedding_dim, self.embedding_dim)
-        self.attention_layer1 = nn.Linear(self.embedding_dim, self.expansion_dim)
-        self.relu = nn.ReLU
-        self.attention_layer2 = nn.Linear(self.expansion_dim, self.embedding_dim)
+        self.attention_block = Attention()
 
         self.decoder = nn.Linear(self.embedding_dim, self.vocab_size)
-
-    def attention(self, x):
-        # Placeholder for the attention mechanism
-        key = self.key_layer(x)
-        query = self.query_layer(x)
-        value = self.value_layer(x)
-        x = self.kvq_math(key, query, value)
-
-        # TODO: Add mask
-
-        # Run through attention MLP
-        x = self.attention_layer1(x)
-        x = self.relu(x)
-        x = self.attention_layer2(x)
-
-        return x
-    
-    def kvq_math(self, key, query, value):
-        
-        # Take cross product of key and query
-        key_transpose = key.transpose(-2, -1) # TODO: Fix tranpose math here
-        cross_attention = torch.matmul(query, key_transpose)
-        cross_attention = torch.softmax(cross_attention)
-        result = torch.multiply(value, cross_attention)
-
-        return result
 
     def tokenizer(self, text):
         # Placeholder for the tokenizer
@@ -70,6 +38,47 @@ class SimpleLLM(nn.Module):
         # Placeholder for the forward pass
         x = self.tokenizer(x)
         x = self.embed_tokens(x)
-        x = self.attention(x)
+        x = self.attention_block(x)
         x = self.decoder(x)
         return x
+
+class Attention(nn.Module):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.key_layer = nn.Linear(self.embedding_dim, self.embedding_dim)
+        self.query_layer = nn.Linear(self.embedding_dim, self.embedding_dim)
+        self.value_layer = nn.Linear(self.embedding_dim, self.embedding_dim)
+        
+        self.attention_layer1 = nn.Linear(self.embedding_dim, self.expansion_dim)
+        self.relu = nn.ReLU
+        self.attention_layer2 = nn.Linear(self.expansion_dim, self.embedding_dim)
+
+
+    # Override foward, so when calling attention(...), this function is called
+    def forward(self, x):
+        # Placeholder for the attention mechanism
+        key = self.key_layer(x)
+        query = self.query_layer(x)
+        value = self.value_layer(x)
+        x = self.kvq_math(key, query, value)
+
+        # TODO: Add mask
+
+        # Run through attention MLP
+        x = self.attention_layer1(x)
+        x = self.relu(x)
+        x = self.attention_layer2(x)
+
+        return x
+
+    def kvq_math(self, key, query, value):
+        
+        # Take cross product of key and query
+        key_transpose = key.transpose(-2, -1) # TODO: Fix tranpose math here
+        cross_attention = torch.matmul(query, key_transpose)
+        cross_attention = torch.softmax(cross_attention)
+        result = torch.multiply(value, cross_attention)
+
+        return result
